1013df117756588ec05c0fb4ec4b9db77bbe81a8
Emmanuel Dominic <ematembu2@gmail.com>
2020-06-12 15:58:59 +0300

    add api passport package service provider

--- config/app.php
+++ config/app.php
@@ -162,6 +162,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        //Passport Package Service Provider
+        Laravel\Passport\PassportServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
