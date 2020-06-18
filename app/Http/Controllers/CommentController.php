5bb473e93335d71ef6f6b1758d4a58d22c0d27ba
Emmanuel Dominic <ematembu2@gmail.com>
2020-06-18 16:42:50 +0300

    implement CRUD for comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,79 +7,87 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function getAllComments(int $messageId) {
+        $comments = Comment::where('message', $messageId)->get();
+        $count = $comments->count();
+        if ($count==0){
+            return response()->json(['message' => 'No Comments'], 404);
+        }
+        return response($comments->toJson(JSON_PRETTY_PRINT), 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function getComment(int $commentId) {
+        if (Comment::where('id', $commentId)->exists()) {
+            $comment = Comment::where('id', $commentId)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($comment, 200);
+        } else {
+            return response()->json([
+                "message" => "Comment not found"
+            ], 404);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function updateComment(Request $request, int $commentId) {
+        if (Comment::where('id', $commentId)->exists()) {
+            $comment = Comment::findOrFail($commentId);
+            if ($comment->owner == Auth::id()){
+                $comment->message = is_null($request['comment']) ? $comment->comment : $request['comment'];
+                $comment->save();
+                return response()->json([
+                    "success" => "comment updated successfully",
+                    "message" => $comment
+                ], 200);
+            }
+            return response()->json([
+                "message" => "sorry, you're not the author"
+            ], 401);
+        } else {
+            return response()->json([
+                "message" => "Comment not found"
+            ], 404);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Comment $comment)
-    {
-        //
+    public function deleteComment ($commentId) {
+        if(Comment::where('id', $commentId)->exists()) {
+            $comment = Comment::findOrFail($commentId);
+            if ($comment->deleted_at!=null){
+                return response()->json([
+                    "error" => "comment already deleted"
+                ], 400);
+            }else{
+                $comment->delete();
+                return response()->json([
+                    "success" => "Comment deleted successfully",
+                    "message" => $comment
+                ], 200);
+            }
+        } else {
+            return response()->json([
+                "message" => "Comment not found"
+            ], 404);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Comment $comment)
-    {
-        //
+    public function destroyComment ($commentId) {
+        if(Comment::where('id', $commentId)->exists()) {
+            $comment = Comment::findOrFail($commentId);
+            if ($comment->owner == Auth::id()){
+                $comment->forceDelete();
+                return response()->json([
+                    "message" => "Comment destroyed successfully"
+                ], 202);
+            }else{
+                return response()->json([
+                    "message" => "sorry, you're not the author"
+                ], 401);
+            }
+        } else {
+            return response()->json([
+                "message" => "Comment not found"
+            ], 404);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Comment $comment)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Comment $comment)
-    {
-        //
-    }
 }
